befd2799c0b6dd7767069fe587e6c87897a00276
Jaša <78748619+jasasonc@users.noreply.github.com>
2026-06-12 11:19:46 +0200

    Standardize packaging to the sdypy sibling-package template

--- sdypy/model/__init__.py
+++ sdypy/model/__init__.py
@@ -1,4 +1,9 @@
-__version__ = "0.1.5"
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("sdypy-model")
+except PackageNotFoundError:  # source checkout without installed metadata
+    __version__ = "0+unknown"
 
 import pyLump as lumped
 from .shell.shell import Shell
