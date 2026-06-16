63928ef45b553e166550696766baa9ff5e7b08f1
GalStanovnik <gs81330@student.uni-lj.si>
2026-06-16 08:53:09 +0200

    sync

--- sdypy/model/__init__.py
+++ sdypy/model/__init__.py
@@ -5,5 +5,6 @@ from .shell.shell import Shell
 from .tetrahedron.tet10 import Tetrahedron
 from .beam.beam import Beam
 from .eigenvalue_solution import solve_eigenvalue
+from .acoustic_external import AcousticExternalProblem
 
 from . import mesh
